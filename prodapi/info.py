bd26a725ad5c5af97f4c17f81e8f41d8f0028caa
Jacob Magnusson <m@jacobian.se>
2021-03-30 17:27:02 +0200

    Fix importlib_metadata.version import on <=py3.7

--- prodapi/info.py
+++ prodapi/info.py
@@ -2,14 +2,14 @@ import os
 import platform
 
 try:
-    from importlib import metadata
+    from importlib.metadata import version as _version_func
 except ImportError:
     # Python 3.7 and lower
-    import importlib_metadata as metadata  # type: ignore
+    from importlib_metadata import version as _version_func  # type: ignore
 
 __all__ = ("name", "version", "node", "environment")
 
 name = __package__
-version = metadata.version(__package__)
+version = _version_func(__package__)
 node = platform.node()
 environment = os.environ.get("APP_ENVIRONMENT", "dev")
